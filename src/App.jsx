98a1e97ce5ef420aae5b39eefbd91fac3514f238
kaloxe <sanchezcarloscasm@gmail.com>
2024-03-25 14:59:40 -0400

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+// import * as React from 'react'
 import "./App.css";
+import Header from './Header';
+import Content from "./Content";
+import Employees from "./Employees";
+import Footer from "./Footer";
 
 export default function App() {
-  return <main>Hello world</main>;
+  return (
+    <div>
+      <Header></Header>
+      <Content></Content>
+      <Employees></Employees>
+      <Footer></Footer>
+    </div>
+  )
 }
